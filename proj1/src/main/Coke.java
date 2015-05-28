587e9adc99c9070ae332411069e92aa9e2bb9d7a
jq.yang@berkeley.edu <jq.yang@berkeley.edu>
2015-05-28 14:56:30 +0800

    init

---
+++ proj1/src/main/Coke.java
@@ -0,0 +1,5 @@
+public class Coke {
+	public static void main(String[] args) {
+		System.out.println("Hello Coke");
+	}
+}
---
+++ proj2/src/main/Soda.java
@@ -0,0 +1,5 @@
+public class Soda {
+	public static void main(String[] args) {
+		System.out.println("Hello Soda");
+	}
+}
